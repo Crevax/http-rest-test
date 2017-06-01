d298a2ce92ea047860cd85ce5355baf7b8658030
CJ Davis <Christopher.Davis@cqlcorp.com>
2017-06-01 12:38:45 -0400

    Add VAC test endpoint

--- server.js
+++ server.js
@@ -39,6 +39,39 @@ server.get('/favicon.ico', function(req, res) {
     })
 })
 
+server.all('/vac-test', function(req, res) {
+    var now = moment();
+    var timestamp = now.format('MMMM Do YYYY, h:mm:ss a');
+    var logMsg = `Request against ${req.method} ${req.originalUrl} at ${now}`;
+    console.log(logMsg);
+    logHeaders(req.headers);
+    logs.unshift({
+        message: logMsg,
+        time: now.valueOf(),
+        method: req.method,
+        headers: req.headers,
+        params: req.params,
+        path: req.path,
+        url: req.originalUrl,
+        query: req.query,
+        body: req.body
+    });
+
+    res.set("Connection", "Keep-Alive");
+
+    res.json({
+        "OrigWidth":1080,
+        "OrigHeight":1733,
+        "SelectedPart":null,
+        "PossibleAttributeValueIDs":[4747,4749,4750,4745,4746,267,241,242,243,244,278,246],
+        "InactivePricingText":null,
+        "ActivePricingText":"$19.50 - $22.50",
+        "FeaturedImageUrl":"//static.veterinaryapparel.com/siteImages/IMAGES/VAC/Scrubs/VAC%20Exclusives/Hearts%20and%20Paws/82174.jpg?maxWidth=275\u0026maxHeight=442",
+        "FeaturedImageZoomUrl":"//static.veterinaryapparel.com/siteImages/IMAGES/VAC/Scrubs/VAC%20Exclusives/Hearts%20and%20Paws/82174.jpg?maxWidth=1000\u0026maxHeight=1607",
+        "AttributeSelectionNames":{"1":null,"2":"XS"}
+    });
+})
+
 server.all('*', function(req, res) {
     var now = moment();
     var timestamp = now.format('MMMM Do YYYY, h:mm:ss a');
