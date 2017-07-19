b7c533fcba0e6368b5c23cb5dbb5f47b1dda9ca3
CJ Davis <cj@cjdavis.me>
2017-07-19 13:10:19 -0400

    Add CORS allowed methods definition

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ server.use(bodyParser.text());
 server.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
